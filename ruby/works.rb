b470811d679f8d1bbbb58f58084c4a3366359f71
NuxiiGit <nuxii09@gmail.com>
2021-10-08 00:11:48 +0100

    more layout changes

--- ruby/works.rb
+++ ruby/works.rb
@@ -1,3 +1,16 @@
+
+##
+# Renders these items inside of a CSS flexbox.
+def render_items(items)
+    out = "<div class=\"flex\">"
+    items.each do |x|
+        out << "<div class=\"centre\"><div class=\"work\">"
+        out << "<div style=\"--img : url('/image/avatar/7.png'); --angle : 10deg;\"></div>"
+        out << "</div></div>"
+    end
+    out + "\n</div>"
+end
+
 works_info = unmarshal_yaml(read("works/list.yaml"))
 vars = binding
 page = read("works/layout.html")
