9db4fc89be4825b76998ff7cc3987f1ecd70bfb7
NuxiiGit <nuxii09@gmail.com>
2021-10-07 01:26:20 +0100

    updated how skills are added

--- ruby/skills.rb
+++ ruby/skills.rb
@@ -1,3 +1,15 @@
+
+##
+# Renders these items inside of a CSS flexbox.
+def render_items(items)
+    out = "<div class=\"flex\">"
+    items.each do |x|
+        out << "<div class=\"record centre\"><code>#{x}</code></div>"
+    end
+    out + "</div>"
+end
+
+skills_info = unmarshal_yaml(read("skills/list.yaml"))
 vars = binding
 page = read("skills/layout.html")
 page = template(page, vars)
