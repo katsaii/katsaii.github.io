888695db1a131a521c645ea60ae6872f576f9ee9
NuxiiGit <nuxii09@gmail.com>
2021-10-08 01:35:31 +0100

    added source links

--- ruby/works.rb
+++ ruby/works.rb
@@ -5,10 +5,21 @@ def render_items(items)
     out = "<div class=\"flex\">"
     items.each do |x|
         image = x["image"]
+        mirrors = x["mirrors"]
+        mirrors = [] if mirrors == nil
+        mirrors << image
         angle = x["angle"]
         angle = 0 if angle == nil
-        out << "<div class=\"centre\"><div class=\"work\">"
+        out << "<div class=\"centre\">"
+        out << "<div class=\"work\">"
         out << "<a href=\"#{image}\"><div style=\"--img : url('#{image}'); --angle : #{angle}deg;\"></div></a>"
+        out << "</div><div class=\"img-caption\">src ="
+        mirrors.each_with_index do |mirror, i|
+            if i != 0
+                out << " |"
+            end
+            out << " <a href=\"#{mirror}\">#{('A'.ord + i).chr}</a>"
+        end
         out << "</div></div>"
     end
     out + "\n</div>"
