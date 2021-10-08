254aa2cf6be4f76623d623542d506bc388bf36f0
NuxiiGit <nuxii09@gmail.com>
2021-10-08 20:24:38 +0100

    refactored art links

--- ruby/works.rb
+++ ruby/works.rb
@@ -1,6 +1,19 @@
 include Math
 
 ##
+# Gets the abbreviated version of this url.
+def abbreviate(url)
+    if url.include?("github") then "GH"
+    elsif url.include?("twitter") then "Tw"
+    elsif url.include?("soundcloud") then "SC"
+    elsif url.include?("newgrounds") then "Ng"
+    elsif url.include?("deviantart") then "DA"
+    elsif url.include?("tumblr") then "T"
+    else nil
+    end
+end
+
+##
 # Renders these items inside of a CSS flexbox.
 def render_items(items)
     out = "<div class=\"flex\">"
@@ -12,17 +25,20 @@ def render_items(items)
         name = "Img" if name == nil
         mirrors = x["mirrors"]
         mirrors = [] if mirrors == nil
-        mirrors << image
         angle = 10 * Math::sin(rad + 1.5)
         out << "<div class=\"centre\">"
         out << "<div class=\"work\">"
         out << "<a href=\"#{image}\"><div style=\"--img : url('#{thumb}'); --angle : #{angle}deg;\"></div></a>"
-        out << "</div><div class=\"img-caption\">#{name} ="
+        out << "</div><div class=\"img-caption\">#{name}"
         mirrors.each_with_index do |mirror, i|
             if i != 0
                 out << " |"
+            else
+                out << " ="
             end
-            out << " <a href=\"#{mirror}\" target=\"_blank\">#{('A'.ord + i).chr}</a>"
+            abbr = abbreviate(mirror)
+            abbr = i if abbr == nil
+            out << " <a href=\"#{mirror}\" target=\"_blank\">#{abbr}</a>"
         end
         out << "</div></div>"
     end
