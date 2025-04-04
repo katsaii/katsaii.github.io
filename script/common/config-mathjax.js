9414f4bc783724cbb476c134c57061cfb7d088f4
katsaii <39774219+katsaii@users.noreply.github.com>
2025-04-04 01:15:58 +0100

    publish origin 21:17:11

---
+++ script/common/config-mathjax.js
@@ -0,0 +1,10 @@
+window.MathJax = {
+	chtml : { scale : 0.9 },
+	svg : { scale : 0.9 },
+	tex : {
+		processEnvironments : false,
+		inlineMath : [["tex$", "$xet"]],
+		displayMath : [["tex$$", "$$xet"]]
+	},
+	options : { enableMenu : false }
+};
